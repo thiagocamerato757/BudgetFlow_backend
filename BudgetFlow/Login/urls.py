4df7f8bda63d92e29c81ee962e4066668a485b0f
thiagocamerato757 <cameratothiago@gmail.com>
2024-11-25 23:17:27 -0300

    Atualiza configurações e dependências
    - Modifica BudgetFlow/BudgetFlow/settings.py
    - Atualiza serializers em BudgetFlow/Login/serializers.py
    - Atualiza URLs em BudgetFlow/Login/urls.py
    - Modifica views em BudgetFlow/Login/views.py
    - Atualiza dependências em requirements.txt

--- BudgetFlow/Login/urls.py
+++ BudgetFlow/Login/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path
-from .views import Userview
+from .views import *
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your Project API",
+        default_version='v1',
+        description="API documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="cameratothiago@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('login/', Userview.as_view(), name='login'),
-    path('logout/', Userview.as_view(), name='logout'),
-    path('register/', Userview.as_view(), name='register'),
+    path('login/', Loginview.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- BudgetFlow/Login/views.py
+++ BudgetFlow/Login/views.py
@@ -3,40 +3,50 @@ from Login.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login as django_login, logout
-from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from rest_framework.request import Request
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_201_CREATED
-
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 @method_decorator(csrf_exempt, name='dispatch')
-class Userview(APIView):
-    """
-    Handles the API requests for user-related operations.
-
-    Methods:
-        - get(request): Handles GET requests to retrieve all users.
-        - post(request): Handles POST requests to create a new user.
-        - delete(request): Handles DELETE requests to log out the user.
-        - put(request): Handles PUT requests to authenticate and log in the user.
-    """
-
-    def get(self, request: Request) -> Response:
+class Loginview(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='Username'),
+                'password': openapi.Schema(type=openapi.TYPE_STRING, description='Password')
+            }
+        ),
+        responses={200: 'User authenticated and logged in successfully', 400: 'Invalid credentials'}
+    )
+    def put(self, request: Request) -> Response:
         """
-        Handles GET requests to retrieve all users.
+        Handle PUT request to authenticate and log in the user.
 
-        :param request: The HTTP request object.
+        This method authenticates the user with the provided username and password,
+        and logs in the user if the credentials are valid.
+
+        :param request: The HTTP request object containing the username and password.
         :type request: Request
-        :return: A Response object containing serialized user data if the user is authenticated,
-                 otherwise an error message with a 401 status code.
+        :return: A response object containing a success message if the user is authenticated and logged in successfully,
+                 or an error message with a 400 status code if the credentials are invalid.
         :rtype: Response
         """
-        if not request.user.is_authenticated:
-            return Response({"error": "User not authenticated"}, status=HTTP_401_UNAUTHORIZED)
-        
-        users = User.objects.filter(username=request.user.username)
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            django_login(request, user)
+            return Response({"success": "User authenticated and logged in successfully"})
+        return Response({"error": "Invalid credentials"}, status=HTTP_400_BAD_REQUEST)
+@method_decorator(csrf_exempt, name='dispatch')    
+class RegisterView(APIView):
+    @swagger_auto_schema(
+        request_body=UserSerializer,
+        responses={201: 'User created successfully', 400: 'Bad Request'}
+    )
     def post(self, request: Request) -> Response:
         """
         Handle POST request to create a new user.
@@ -61,29 +71,17 @@ class Userview(APIView):
             user.save()
             return Response({"success": "User created successfully"}, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
-    
-    def put(self, request: Request) -> Response:
-        """
-        Handle PUT request to authenticate and log in the user.
-
-        This method authenticates the user with the provided username and password,
-        and logs in the user if the credentials are valid.
-
-        :param request: The HTTP request object containing the username and password.
-        :type request: Request
-        :return: A response object containing a success message if the user is authenticated and logged in successfully,
-                 or an error message with a 400 status code if the credentials are invalid.
-        :rtype: Response
-        """
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            django_login(request, user)
-            return Response({"success": "User authenticated and logged in successfully"})
-        return Response({"error": "Invalid credentials"}, status=HTTP_400_BAD_REQUEST)
-
-    
+@method_decorator(csrf_exempt, name='dispatch')
+class LogoutView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING, description='Username')
+            }
+        ),
+        responses={200: 'User logged out successfully', 401: 'User not authenticated'}
+    )
     def delete(self, request: Request) -> Response:
         """
         Handle DELETE request to log out the user.
