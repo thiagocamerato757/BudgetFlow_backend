974986034800c04b5972fb25600159817af3a54c
MTuro <marcosturo69@gmail.com>
2024-11-28 23:27:05 -0300

    funcoes pra o grafico no models e alteracoes no html

--- BudgetFlow/Gastos/models.py
+++ BudgetFlow/Gastos/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Despesa, Receita
+from .serializers import DespesaSerializer, ReceitaSerializer
+from django.urls import path
+from .views import get_despesas, get_receitas
 
 class Despesa(models.Model):
     CATEGORIAS_DESPESAS = [
@@ -43,4 +49,21 @@ class Receita(models.Model):
 
     def __str__(self):
         return f'{self.descricao} - {self.valor} ({self.categoria})'
-    
+
+@api_view(['GET'])
+def get_despesas(request):
+    despesas = Despesa.objects.all()
+    serializer = DespesaSerializer(despesas, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_receitas(request):
+    receitas = Receita.objects.all()
+    serializer = ReceitaSerializer(receitas, many=True)
+    return Response(serializer.data)
+
+
+urlpatterns = [
+    path('despesas/', get_despesas, name='get_despesas'),
+    path('receitas/', get_receitas, name='get_receitas'),
+]
